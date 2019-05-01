6ffc9d1497810dc2ad70b127cc4da9eeb3b3fb3f
Vishwesh Rathi <vishweshsr@gmail.com>
2019-05-01 14:49:31 +0530

    Added new logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux'
-import * as classes from './Layout.css'
-import logo from '../../../assets/images/main_logo.jpg'
+// import classes from './Layout.css'
+// import logo from '../../../public/images/logo_dia.png'
 class Layout extends Component {
-    render () {
+    render() {
         return (
             <Aux>
-                <div className={classes.main_frame}>
-                    <div className={classes.header}>
-                    <img className={classes.Logo} src={logo} alt="Diagnocare" />                    
-                    <div className={classes.header_text}>DIAGNOCARE ENTERPRISES</div>
+                {/* <nav className="navbar navbar-default navbar-fixed-top">
+                    <div className="container">
+                        <button type="button"
+                            className="navbar-toggle"
+                            data-toggle="collapse"
+                            data-target=".navbar-collapse">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="www.google.com">DIAGNOCARE ENTERPRISES</a>
+                        <div className="navbar-collapse collapse">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li className="active"><a href="/home" > Home</a></li>
+                                { <li ><a href="/products" > Products</a></li> }
+
+                                <li className="dropdown">
+                                
+                                </li>
+                                <li ><a href="/contactus" > Contact us</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <main>
-                        {this.props.children}
-                    </main>
-                </div>
-            </Aux>
+                </nav>
+ */}
+
+                <main>
+                    {this.props.children}
+                </main>
+            </Aux >
         );
     };
 
